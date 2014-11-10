f94c377d741887447357b9af32ead8906913ba51
Portia Burton <plburton@gmail.com>
2014-11-09 18:43:03 -0800

    Added GET rows

--- streaming_dat.py
+++ streaming_dat.py
@@ -1,7 +1,24 @@
 import json
 import requests
 
-r = requests.get('http://localhost:6461/api', stream=True)
+port = 'http://localhost:6461'
 
-print r.content
+def get_dat():
+    try:
+        call = port + '/api'
+        req = requests.get(call, stream=True)
+        print(req.content)
+    except:
+        raise Exception("You are not Connected to DAT. Please enter \'dat listen\' ")
+    return get_dat
+get_dat()         
 
+def get_datastore():
+    try:
+        call = port + '/api/rows'
+        req = requests.get(call, stream=True)
+        print(req.content)
+    except:
+        raise Exception("You are not connected to DAT. Please enter \'dat listen\' ")
+    return get_datastore
+get_datastore()
