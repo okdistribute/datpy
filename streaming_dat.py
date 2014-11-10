09f3c2a084140fcc18fad8a42224a33f3118b2df
Portia Burton <plburton@gmail.com>
2014-11-09 22:09:40 -0800

    Get changes

--- streaming_dat.py
+++ streaming_dat.py
@@ -16,15 +16,14 @@ get_dat()
 
 def get_datastore():
 # json representation of dat
-	try:
-		call = port + '/api/row'
+    try:
+        call = port + '/api/row'
         req = requests.get(call, stream=True)
         print(req.content)
     except:
         raise Exception("You are not connected to DAT. Please enter \'dat listen\' ")
     return get_datastore
 get_datastore()   
-
 
 def get_session():
     # get session info
@@ -36,3 +35,14 @@ def get_session():
         raise Exception("You are not connected to DAT. Please enter \'dat listen \' ")
     return get_session
 get_session()
+
+def get_changes():
+    # get changes
+    try:
+        call = port + '/api/changes'
+        req = requests.get(call, stream=True)
+        print(req.content)
+    except:
+        raise Exception("You are not connected to DAT. Please enter \'dat listen\' ")
+    return get_changes
+get_changes()
