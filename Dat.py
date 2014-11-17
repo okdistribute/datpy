a2ec918a4dd5872cfc01861e357fa35f0ae2a338
Portia Burton <plburton@gmail.com>
2014-11-16 22:28:13 -0800

    Added info method

--- Dat.py
+++ Dat.py
@@ -3,18 +3,12 @@ import json
 
 class Dat:
     def __init__(self, host):
-        req = requests.get(host, stream=True)
+        self.req = requests.get(host, stream=True)
         self.host = host
-        #self.session = requests.get(host, stream=True)  
+        self.api_base = '{}/api'.format(self.host)
 
-    def dat_info(host):
-        call = str(host + '/api')
-        return dat_info
-        print (req.content)
-    
-    
-    def dat_diff(host):
-        call = host + '/api/changes'
-        return dat_info
-        print (req.content)
+    def info(self):
+        req = requests.get(self.api_base, stream=True)
+        print(req.content)
+
 
