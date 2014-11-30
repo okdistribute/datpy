3900f0d45f0a4dd8524333edce8dd4e4e6a7c06a
Portia Burton <plburton@gmail.com>
2014-11-29 22:39:27 -0800

    Added post method

--- dat.py
+++ dat.py
@@ -36,3 +36,9 @@ class Dat:
         req = requests.get(call, stream=True)
         dat_dic = json.loads(req.text)
         print dat_dic
+
+    def post(self, filename):
+        call = '{}/rows'.format(self.api_base)
+        with open(filename, 'rb') as f:
+            req = requests.post(call, data=f)
+        print(req.content)
