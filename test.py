e8a333f3e6d866ecac091a045848660d45a45377
Portia Burton <plburton@gmail.com>
2014-12-13 00:18:28 -0800

    Added rows test, updated info test

--- test.py
+++ test.py
@@ -9,10 +9,21 @@ def info():
     print(req.content)
     return req.status_code
 
+def rows():
+    call = port + '/api/rows'
+    req = requests.get(call, stream=True)
+    print(req.content)
+    return req.headers['content-type']
+
 class DatTest(unittest.TestCase):
 
     def test_info(self):
     	self.assertEqual(info(), 200)
+
+    def test_rows(self):
+    	self.assertEqual(rows(),'application/json')
+
+
         
 if __name__ == '__main__':
     unittest.main()
