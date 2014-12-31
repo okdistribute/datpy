0c0dbc6358da6bd8ce35991c2d4acb1b063281c3
Karissa McKelvey <krmckelv@gmail.com>
2014-12-30 16:40:28 -0800

    CLN: return print statements

--- dat.py
+++ dat.py
@@ -10,37 +10,31 @@ class Dat:
 
     def info(self):
         req = requests.get(self.api_base, stream=True)
-        print(req.content)
         return req.content
 
     def diff(self):
         call = '{}/changes'.format(self.api_base)
         req = requests.get(call, stream=True)
-        print(req.content)
         return req.content
 
     def session(self):
         call = '{}/session'.format(self.api_base)
         req = requests.get(call, stream=True)
-        print(req.content)
         return req.content
 
     def csv(self):
         call = '{}/csv'.format(self.api_base)
         req = requests.get(call, stream=True)
-        print(req.content)
         return req.content
 
     def rows(self):
         call = '{}/rows'.format(self.api_base)
         req = requests.get(call, stream=True)
-        print(req.content)
         return req.content
 
     def dict(self):
         call = '{}/rows'.format(self.api_base)
         req = requests.get(call, stream=True)
-        dat_dic = json.loads(req.text)
         print dat_dic
         return req.content
 
@@ -48,9 +42,8 @@ class Dat:
         call = '{}/rows'.format(self.api_base)
         with open(filename, 'rb') as f:
             req = requests.post(call, data=f)
-        print(req.content)
         return req.content
-        
+
     def post_csv(self, filename):
         call = '{}/bulk?results=true'.format(self.api_base)
         headers = {
@@ -58,5 +51,4 @@ class Dat:
         }
         with open(filename) as f:
             req = requests.post(call, data=f, headers=headers)
-        print(req.content)
         return req.content
