e84fb7f6f1b7c3c99d11ddac81eb531dbb41a1d8
Karissa McKelvey <krmckelv@gmail.com>
2015-08-05 01:19:30 -0400

    0.5.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setup(
   name = 'datpy',
-  version = '0.5',
+  version = '0.5.1',
   py_modules=['datpy'],
   description = 'Python dough for making Dat-flavored pies',
   author = 'Karissa McKelvey',
   author_email = 'krmckelv@gmail.com',
   url = 'http://dat-data.com',
-  download_url = 'https://github.com/karissa/datpy/tarball/0.5',
+  download_url = 'https://github.com/karissa/datpy/tarball/0.5.1',
   keywords = ['dat', 'python', 'analytics', 'data sharing'],
   classifiers = []
 )
