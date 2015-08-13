ed1ba345b961f7d4fa21bede16c25a1bd590782a
Karissa McKelvey <krmckelv@gmail.com>
2015-08-13 12:02:21 -0400

    add 2.6 to list of supported python versions

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     keywords=['dat', 'python', 'analytics', 'data sharing'],
     classifiers=(
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
