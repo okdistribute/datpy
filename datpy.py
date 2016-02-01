a858337a1d1536f7f836a03648a8d3d92d767632
Karissa McKelvey <krmckelv@gmail.com>
2016-01-31 19:36:42 -0500

    update datpy for dat 1.0

--- datpy.py
+++ datpy.py
@@ -17,7 +17,7 @@ except:
   pd = False
 
 
-COMPATIBLE_DAT_VERSION = '7.1.0'
+COMPATIBLE_DAT_VERSION = '8'
 
 class DatException(Exception):
   pass
@@ -28,138 +28,51 @@ def on_error(log):
     message = 'Unknown error. Please contact us at #dat in freenode on irc'
   raise DatException('Error: ' + message)
 
-def returns_version(func):
-  def inner(*args, **kwargs):
-    self = args[0]
-    log = func(*args, **kwargs)
-    self.version = log.get("version")
-    if not self.version:
-      on_error(log)
-    return self.version
-
-  return inner
-
-def clone(URL, path=None, **kwargs):
-  """
-  Parameters
-  """
-  if not path:
-    path = ''
-  p = process("dat clone {0} {1}".format(URL, path), kwargs)
-  out = stream_out(p)
-  return Dat(path)
-
-
 class Dat(object):
 
-  def __init__(self, path=None):
-    self.path = path
-    self.version = None
+  def __init__(self, home=None):
+    self.home = home
+    self._opened = []
 
-  def init(self, **kwargs):
-    p = self.process("dat init --no-prompt", kwargs)
-    return stream_out(p)
+  def close(self, pid=None):
+    for p in self._opened:
+      if not pid:
+        subprocess.Popen.terminate(p)
+      if pid == p.pid:
+        subprocess.Popen.terminate(p)
+    return True
 
-  def checkout(self, version, **kwargs):
-    self.version = version
-    p = self.process("dat checkout " + version, kwargs)
-    return stream_out(p)
+  def link(self, path):
+    p = self._call('link {0}'.format(path))
+    for line in iter(p.stdout.readline, b''):
+      line = line.decode()
+      index = line.find('dat://')
+      if index > -1:
+        self._opened.append(p)
+        return line[index:].strip()
+    return p
 
-  def datasets(self, **kwargs):
-    p = self.process("dat datasets", kwargs)
-    res = stream_out(p)
-    return res['datasets']
+  def download(self, link, path=None):
+    opts = {}
+    if path:
+      opts['path'] = path
+    p = self._call(link, opts)
+    for line in iter(p.stdout.readline, b''):
+      line = line.decode()
+      print line
+      if line.find('Download complete') > -1:
+        self._opened.append(p)
+        return True
+    return False
 
-  def files(self, **kwargs):
-    p = self.process("dat files", kwargs)
-    res = stream_out(p)
-    return res['files']
+  def metadata(self):
+    return self._call('metadata')
 
-  def destroy(self, **kwargs):
-    p = self.process("dat destroy --no-prompt", kwargs)
-    return stream_out(p)
-
-  def status(self, **kwargs):
-    p = self.process("dat status", kwargs)
-    return stream_out(p)
-
-  def read(self, key, **kwargs):
-    p = self.process("dat read " + key, kwargs)
-    return stream_out(p, parse=False)
-
-  @returns_version
-  def write(self, filename, data=None, **kwargs):
-    p = self.process("dat write {0} -".format(filename), kwargs)
-    return stream_in(p, data)
-
-  @returns_version
-  def write_file(self, filename, **kwargs):
-    p = self.process("dat write " + filename, kwargs)
-    return stream_out(p)
-
-  def process(self, cmd, opts):
-    if self.path:
-      opts['path'] = self.path
-
-    return process(cmd, opts)
-
-  def dataset(self, name):
-    return Dataset(self, name)
-
-class Dataset(object):
-
-  def __init__(self, dat, dataset, key=False):
-    self.dat = dat
-    self.dataset = dataset
-    self.key = key
-    if not self.key:
-      self.key = 'key'
-
-  def keys(self, **kwargs):
-    p = self.process("dat keys", kwargs)
-    res = stream_out(p)
-    if 'keys' in res:
-      return res['keys']
-    return res
-
-  @returns_version
-  def import_file(self, filename, **kwargs):
-    p = self.process("dat import " + filename, kwargs)
-    return stream_out(p)
-
-  @returns_version
-  def import_dataframe(self, dataframe, **kwargs):
-    ## TODO: make streaming better by using a generator
-    key = kwargs.get('key')
-    if key:
-      self.key = key
-    p = self.process("dat import -", kwargs)
-    return stream_in(p, dataframe.to_csv())
-
-  def export_dataframe(self, **kwargs):
-    if not pd:
-      raise Exception("Can't find pandas. Is it available on your path?")
-
-    output = self.export(**kwargs)
-    frame = pd.DataFrame.from_dict(output)
-    return frame
-
-  def export(self, **kwargs):
-    kwargs['full'] = True
-    p = self.process("dat export", kwargs)
-    output = stream_out(p)
-    res = []
-    for row in output:
-      row['value'][self.key] = row['key']
-      res.append(row['value'])
-    return res
-
-  def process(self, cmd, opts):
-    if self.dat.path:
-      opts['path'] = self.dat.path
-
-    opts['dataset'] = self.dataset
-
+  def _call(self, cmd, opts=None):
+    if opts is None:
+      opts = {}
+    if self.home:
+      opts['home'] = self.home
     return process(cmd, opts)
 
 def process(cmd, opts):
@@ -177,7 +90,7 @@ def process(cmd, opts):
   if opts is None:
     opts = {}
 
-  cmd += ' --json '
+  cmd = 'dat ' + cmd
 
   for key, val in opts.items():
     if (len(key) == 1):
--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import json
-import pickle
 import unittest
+import os
 
 import datpy
 
@@ -12,153 +11,30 @@ try:
 except:
   pd = False
 
+path = 'example-download-data'
+
 class DatTest(unittest.TestCase):
 
   @classmethod
   def setUpClass(cls):
-    cls.dat = datpy.Dat('.')
-    cls.dat.init()
+    cls.dat = datpy.Dat()
 
-  @classmethod
   def tearDownClass(cls):
-    cls.dat.destroy()
-
-class IsolatedTest(DatTest):
-
-  def test_insert_with_dataset(self):
-    dataset = self.dat.dataset('contracts')
-    version = dataset.import_file("examples/contracts.csv")
-    self.assertEqual(len(version), 64)
-    output = dataset.export()
-    self.assertEqual(len(output), 770)
-    self.assertTrue('amtSpent' in output[0])
-
-    status = self.dat.status()
-    self.assertEqual(len(status['datasets']), 1)
-    self.assertEqual(status['files'], 1)
-    #self.assertEqual(status['rows'], 770)
-
-    dat = datpy.clone('.', 'test')
-    self.assertEqual(status, dat.status())
-    dat.destroy()
-
-    datasets = self.dat.datasets()
-    self.assertEqual(len(datasets), 1)
-    self.assertTrue('contracts' in datasets)
-
+    cls.dat.close()
+    os.rmdir(path)
 
 class IOTests(DatTest):
 
-  def test_insert_with_dataset(self):
-    dataset = self.dat.dataset('contracts')
-    version = dataset.import_file("examples/contracts.csv")
-    self.assertEqual(len(version), 64)
-    output = dataset.export()
-    self.assertEqual(len(output), 770)
+  def test_link(self):
+    self.link = self.dat.link('examples')
+    self.assertTrue(link.startswith('dat://'))
+    self.assertEquals(len(link.replace('dat://')), 64)
+    self.assertEquals(len(dat._opened), 1)
 
-  def test_insert_with_bad_path_fails(self):
-    dataset = self.dat.dataset('contracts2')
-    with self.assertRaises(Exception):
-        dataset.import_file("not-a-file.csv")
-
-  def test_write_file(self):
-    version = self.dat.write_file("examples/blob.txt")
-    self.assertEqual(len(version), 64)
-    output = self.dat.read("blob.txt")
-    self.assertEqual(output, "hello world\n")
-
-  def test_write_blob_from_python(self):
-    version = self.dat.write("hello.txt", data="hello world")
-    self.assertEqual(len(version), 64)
-    self.assertEqual(self.dat.read("hello.txt"), "hello world")
-
-  def test_write_dict_from_python(self):
-    my_python_object = {
-      "hello": "world",
-      "goodbye": "mars"
-    }
-    binary_data = json.dumps(my_python_object)
-    version = self.dat.write("helloworld_dict", data=binary_data)
-    self.assertEqual(len(version), 64)
-
-    out_data = self.dat.read("helloworld_dict")
-    output = json.loads(out_data)
-    self.assertEqual(type(output), dict)
-    self.assertEqual(output["hello"], "world")
-
-  def test_write_pickle_from_python(self):
-    my_python_object = {
-      "hello": "mars",
-      "goodbye": "world"
-    }
-    data = pickle.dumps(my_python_object)
-    version = self.dat.write("helloworld.pickle", data=data)
-    self.assertEqual(len(version), 64)
-
-    data = self.dat.read("helloworld.pickle")
-    if isinstance(data, dict):
-        obj = data
-    else:
-        obj = pickle.loads(data)
-    self.assertEqual(type(obj), dict)
-    self.assertEqual(obj["hello"], "mars")
-
-  def test_get_datasets(self):
-    dataset = self.dat.dataset('test_get_datasets')
-    version = dataset.import_file("examples/contracts.csv")
-    output = self.dat.datasets()
-    self.assertEqual(len(output), 1)
-
-  def test_get_files(self):
-    version = self.dat.write_file("examples/blob.txt")
-    output = self.dat.files()
-    self.assertEqual(output[-1], 'package.json')
-    self.assertEqual(len(output), 2)
-
-@unittest.skipIf(pd is False, "skipping pandas tests")
-class TestPandas(DatTest):
-
-  def test_pandas(self):
-    # clean column, turn into float
-    df = pd.read_csv('examples/contracts.csv')
-    self.assertEquals(df.shape, (770, 10))
-
-    df['amtSpent'] = df['amtSpent'].str.replace(r'[$,]', '')
-
-    # insert data
-    dataset = self.dat.dataset("pandas")
-    version = dataset.import_dataframe(df)
-    self.assertEqual(64, len(version))
-
-    output = dataset.export()
-    self.assertEqual(len(output), 770)
-
-    df = dataset.export_dataframe()
-
-    # modify a column
-    # create ranked column.
-    df['amtSpentRank'] = df['amtSpent'].rank()
-    self.assertEquals(df.shape, (770, 13))
-
-    # okay, put it back in dat
-    version = dataset.import_dataframe(df, key='key')
-    self.assertEqual(len(version), 64)
-
-    # and get it back out
-    output = dataset.export()
-    self.assertEqual(len(output), 770)
-    df_with_rank = pd.DataFrame.from_dict(output)
-
-    # get the new data in a data frame.
-    # we should see the updated data and new column there.
-    self.assertEquals(df_with_rank.shape, (770, 13))
-
-    # do some type conversion
-    # TODO: save dtypes and automagically parse them for the python user
-    # if coming to/from pandas
-    df_with_rank['amtSpentRank'] = df_with_rank['amtSpentRank'].astype('float')
-
-    self.assertTrue(df_with_rank['amtSpentRank'].equals(df['amtSpentRank']))
+  def test_download(self):
+    self.dat.download(self.link, path)
+    self.assertEquals(os.listdir(path), os.listdir('examples'))
+    self.assertEquals(len(dat._opened), 2)
 
 if __name__ == '__main__':
   unittest.main()
