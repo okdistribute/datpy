2cb31824d0db5b6b716cd958479c175071316472
Joe Hand <joe@joeahand.com>
2016-02-02 09:20:24 -0800

    update tests to work w/ python 3 and run consistent across versions

--- tests/test.py
+++ tests/test.py
@@ -3,39 +3,44 @@ from __future__ import unicode_literals
 
 import unittest
 import os
+import sys # Remove this
 
 import datpy
 
-try:
-  import pandas as pd
-except:
-  pd = False
+EXAMPLE_DIR = 'examples'
 
-path = 'example-download-data'
+root = os.path.dirname(datpy.__file__)
+linkPath = os.path.join(root,EXAMPLE_DIR)
+downloadDir = os.path.join(os.path.join(root,'example-download-data'))
 
 class DatTest(unittest.TestCase):
 
   @classmethod
   def setUpClass(cls):
+    if not os.path.exists(downloadDir):
+      os.makedirs(downloadDir)
+    cls.downloadPath = os.path.join(downloadDir, EXAMPLE_DIR)
     cls.dat = datpy.Dat()
 
   @classmethod
   def tearDownClass(cls):
     cls.dat.close()
-    files = os.listdir(path + '/examples')
+    files = os.listdir(cls.downloadPath)
     for filepath in files:
-      os.remove(path + '/examples/' + filepath)
-    os.rmdir(path + '/examples')
+      os.remove(os.path.join(cls.downloadPath,filepath))
+    os.rmdir(cls.downloadPath)
+    os.rmdir(downloadDir)
 
 class IOTests(DatTest):
 
   def test_link_and_download(self):
-    link = self.dat.link('examples')
+    print(sys.version)
+    link = self.dat.link(linkPath)
     self.assertTrue(link.startswith('dat://'))
     self.assertEquals(len(link.replace('dat://', '')), 64)
     self.assertEquals(len(self.dat._opened), 1)
-    self.dat.download(link, path)
-    self.assertEquals(os.listdir(path + '/examples'), os.listdir('examples'))
+    self.dat.download(link, path=self.downloadPath)
+    self.assertEquals(os.listdir(self.downloadPath), os.listdir(linkPath))
     self.assertEquals(len(self.dat._opened), 2)
 
 if __name__ == '__main__':
